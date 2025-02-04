139b98a8bc294725d1cf7bbb6a2c0638a91f2699
AntonSkyrda <mr.skyrda@gmail.com>
2025-02-04 22:03:14 +0200

    solution

--- courses/urls.py
+++ courses/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 
-from .views import CourseListView, CourseDetailView, IndexView
+from .views import CourseListView, CourseDetailView, IndexView, CourseStudentsListView
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     path("courses/", CourseListView.as_view(), name="course_list"),
     path("courses/<int:pk>/", CourseDetailView.as_view(), name="course_detail"),
+    path(
+        "courses/<int:course_id>/students/",
+        CourseStudentsListView.as_view(),
+        name="course_students_list",
+    ),
 ]
 
 app_name = "courses"
--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from django.views.generic import ListView, DetailView, TemplateView
 
 from .models import Course
@@ -23,3 +24,23 @@ class CourseListView(ListView):
 
 class CourseDetailView(DetailView):
     model = Course
+
+
+class CourseStudentsListView(ListView):
+    template_name = "courses/students_list.html"
+    context_object_name = "students"
+
+    def get_queryset(self):
+        course = get_object_or_404(Course, id=self.kwargs["course_id"])
+        students = course.get_students_with_marks()
+
+        sort_order = self.request.GET.get("sort", "desc")
+        if sort_order == "asc":
+            students = students.order_by("total_marks")
+
+        return students
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["course"] = get_object_or_404(Course, id=self.kwargs["course_id"])
+        return context
